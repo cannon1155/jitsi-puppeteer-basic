0ee84a7cff69ba7ed7f1bc5d9883c9796effc1dc
James-Wickenden <jameswickenden14@gmail.com>
2021-10-27 09:52:07 +0100

    updated script to work with subprocess

--- scale_clients.py
+++ scale_clients.py
@@ -1,13 +1,13 @@
 import time
-import os
+import subprocess
 
 print('beginning experiment...')
 
 for i in range(15):
     no_clients = i+1
-    print(f'\n=== RUNNING WITH {no_clients} CLIENTS ===')
-    os.system(f'sudo docker-compose up --scale jitsi-puppet-video={no_clients}')
-    time.sleep(3600) # wait an hour for the measurements to be taken
+    print(f'\n=== RUNNING WITH {no_clients} CLIENTS === ')
+    subprocess.run(['sudo', 'docker-compose', 'up', '--scale', 'jitsi-puppet-video=' + str(no_clients)])
+    print(f' ...done. Waiting for cooldown.')
     time.sleep(120)  # wait two minutes for the machine to return to baseline
 
 print('finished experiment.')
