1599c267e4aba309d1c0ef5389e46fa36a8a68f7
James-Wickenden <jameswickenden14@gmail.com>
2021-10-29 14:08:43 +0100

    don't create 6.0 images with docker-compose up

--- scale_clients.py
+++ scale_clients.py
@@ -7,7 +7,7 @@ subprocess.run(['xhost', '+'])
 for i in range(15):
     no_clients = i+1
     print(f'\n=== RUNNING WITH {no_clients} CLIENTS === ')
-    subprocess.run(['docker-compose', 'up', '--scale', 'jitsi-full=' + str(no_clients)])
+    subprocess.run(['docker-compose', 'up', '--scale', 'jitsi-full=' + str(no_clients), '--scale', 'jitsi-puppet-video=0'])
     print(f' ...done. Waiting for cooldown.')
     time.sleep(120)  # wait two minutes for the machine to return to baseline
 
